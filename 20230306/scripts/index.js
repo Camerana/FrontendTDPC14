d122cf1b0fe4792c89033a275563bcc876495b89
daniepa <daniloepatti@gmail.com>
2023-03-06 19:58:48 +0100

    lezione del 6/3

--- 20230306/scripts/index.js
+++ 20230306/scripts/index.js
@@ -14,6 +14,8 @@ btn1.onclick = function() {
     for(let i = 0; i < paragrafi.length; i++) {
         // per ogni elemento..
         paragrafi[i].classList.add("evidenzia");
+        // paragrafi[i].classList.add("text-warning");
+        // console.log(paragrafi[i].classList);
     }
 }
 
@@ -36,13 +38,54 @@ btn3.onclick = function() {
 const listaPersone = ["Corrado", "Piera", "Renzo", "Rossella"];
 const listaUl = document.getElementById("lstPersone");
 
+const btn4 = document.getElementById("btn4");
+const inputNome = document.getElementById("nome");
+const inputPwd = document.getElementById("pwd");
+
+btn4.onclick = function() {
+    const nome = inputNome.value;
+    // const pwd = inputPwd.value;
+    // if(nome == "") {
+    //     alert("non hai inserito il nome")
+    // } 
+    // else {
+    //     alert("ciao " + nome);
+    //     console.log(pwd);
+    // }
+    if(nome != "") {
+        listaPersone.push(nome);
+        aggiornaLista();
+        inputNome.value = "";
+    }
+}
+
 aggiornaLista();
 
 function aggiornaLista() {
-    listaUl.innerText = "";
+    listaUl.innerText= "";
+
     for(let i = 0; i < listaPersone.length; i++) {
-        listaUl.innerHTML += `
-            <li>${listaPersone[i]}</li>
-        `;
+        // listaUl.innerHTML += `
+        //     <li>${listaPersone[i]}</li>
+        // `;
+        const itemPersona = document.createElement("li");
+        itemPersona.innerHTML = `${listaPersone[i]}`;
+        listaUl.appendChild(itemPersona);
     }
 }
+
+const div = document.getElementById("sezione3");
+
+// 1. creare h2
+const intestazione = document.createElement("h2");
+// 2. valorizzare suo contenuto
+intestazione.innerText = "HAHAHA";
+
+const paragrafo = document.createElement("p");
+paragrafo.innerText = "hahaha";
+
+// 3. appendere al div l'h2
+div.appendChild(intestazione);
+div.appendChild(paragrafo);
+
+
